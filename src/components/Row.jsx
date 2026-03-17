e189a5fd54d460a72f40616aafb32e2d32b80820
Andres Garcia <andres.inginformatico@gmail.com>
2026-03-17 12:48:31 -0500

    feat: Add Row component to display movie posters and play trailers with new dependencies and styling.

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+  import React, { useState, useEffect } from 'react';
 import axios from '../api/axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 import './Row.css';
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -16,6 +19,27 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -27,6 +51,7 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
               (!isLargeRow && movie.backdrop_path)) && (
               <img
                 key={movie.id}
+                onClick={() => handleClick(movie)}
                 className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                 src={`${base_url}${
                   isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -36,6 +61,7 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             )
         )}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
